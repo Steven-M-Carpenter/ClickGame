7ebc70d7ec9676929515230019e69f4fff2b9c51
SMCarpenter <smc1155@gmail.com>
2018-12-23 12:25:52 -0500

    Updates

---
+++ precache-manifest.9c166e8cd223b2c97c7f7aade7105720.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ed944e191ea79bced1d",
+    "url": "/Steven-M-Carpenter/clickgame/static/js/runtime~main.0ed944e1.js"
+  },
+  {
+    "revision": "d984d0ba01e3a9ba65bd",
+    "url": "/Steven-M-Carpenter/clickgame/static/js/main.d984d0ba.chunk.js"
+  },
+  {
+    "revision": "70095bf0b7174c578892",
+    "url": "/Steven-M-Carpenter/clickgame/static/js/1.70095bf0.chunk.js"
+  },
+  {
+    "revision": "d984d0ba01e3a9ba65bd",
+    "url": "/Steven-M-Carpenter/clickgame/static/css/main.aa4430ff.chunk.css"
+  },
+  {
+    "revision": "c1eb803ae95db6482278bf8e999f8863",
+    "url": "/Steven-M-Carpenter/clickgame/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Steven-M-Carpenter/clickgame/precache-manifest.9c166e8cd223b2c97c7f7aade7105720.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Steven-M-Carpenter/clickgame/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
